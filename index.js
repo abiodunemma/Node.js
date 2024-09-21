4298ce275f3564ee4a92b5053ee28dfd5c98a042
abiodun emma <abiodunemma769@gmail.com>
2024-09-22 00:05:56 +0100

    pipe data passage

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
+// const http = require('node:http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+// myReadStream.pipe(myWriteStream);
 
 
-const http = require('node:http');
-var fs = require('fs');
+//we can use pipe instead
+// myReadStream.on('data', function(chunk){
+//   console.log('new chunk received:');
+//   myWriteStream.write(chunk);
+//   //console.log(chunk);
+// })
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-
-myReadStream.on('data', function(chunk){
-  console.log('new chunk received:');
-  console.log(chunk);
-})
 
 
 // const hostname = '127.0.0.1';
@@ -24,3 +29,24 @@ myReadStream.on('data', function(chunk){
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'content-Type': 'application/json'});
+  var myObj = {
+    name: 'Ryu',
+    job: 'Ninja',
+    age: 29 
+};
+res.end(JSON.stringify(myObj));
+
+});
+
+// for running html page
+  // var myReadStream = fs.createReadStream(__dirname + '/home.html', 'utf8');
+  // myReadStream.pipe(res);
+
+ server.listen(3000, '127.0.0.1');
+ console.log('lolllllllll',);
--- writeMe.txt
+++ writeMe.txt
@@ -1,1 +1,43 @@
-yay, you read me !
+# Project Title
+
+A brief description of your project, its purpose, and what problem it solves.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [API Endpoints](#api-endpoints)
+- [Contributing](#contributing)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Introduction
+
+This is my first Node.js project, aimed at [describe the purpose of your project]. With this project, I intend to [explain your goals and what you hope to achieve].
+
+## Features
+
+- Feature 1: [Brief description]
+- Feature 2: [Brief description]
+- Feature 3: [Brief description]
+- [Add any additional features]
+
+## Technologies Used
+
+- **Node.js**: JavaScript runtime built on Chrome's V8 engine.
+- **Express**: Fast, unopinionated, minimalist web framework for Node.js.
+- **MongoDB** (or other databases): For data storage.
+- **Mongoose**: MongoDB object modeling for Node.js.
+- **Other Libraries/Technologies**: [List any other libraries, frameworks, or tools you used.]
+
+## Installation
+
+Follow these steps to get your development environment set up:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yourusername/your-repo-name.git
