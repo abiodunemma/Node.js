caa72822bb6fba3fe62bcbb73d31d0914bfc3d09
abiodun emma <abiodunemma769@gmail.com>
2024-09-21 14:51:55 +0100

    updated readme

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+
+
+
 const http = require('node:http');
+var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+myReadStream.on('data', function(chunk){
+  console.log('new chunk received:');
+  console.log(chunk);
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
