d8a91906ad73161705c8b1dffaa7fbb3b1b11202
abiodun emma <abiodunemma769@gmail.com>
2025-01-02 00:05:53 +0100

    api_routing

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
+
 app.set('view engine', 'ejs');
+app.use('/assests', express.static('assests'));
+
 
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/home.html');  // Serve home.html
+    res.render('home');  
 });
 
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');  // Serve contact.html
+
+  res.render('contact', {qs: req.query});
 });
 
+app.post('/contact', urlencodedParser, function(req, res){
+console.log(req.body);
+    res.render('contact-success', {data: req.body});
+  });
+
 app.get('/profile/:name', function(req, res){
-    res.render('profile', { person: req.params.name });  
+    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile', { person: req.params.name, data: data});  
 });
 app.listen(3000);
     
